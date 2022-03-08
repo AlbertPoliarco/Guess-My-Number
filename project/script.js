05907b48a8bdbe5704afd1fcd399ef5075370e8c
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-03-08 14:14:58 -0600

    added score

--- project/script.js
+++ project/script.js
@@ -12,6 +12,8 @@ console.log(document.querySelector('.guess').value);
 
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -23,8 +25,12 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High';
+    document.querySelector('.message').textContent = '📈 Too High';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
+    document.querySelector('.message').textContent = '📉 Too Low';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
