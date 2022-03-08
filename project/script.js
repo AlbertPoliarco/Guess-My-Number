a5a3d550147e0b19c3b67cc8fac338fb34fd5033
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-03-08 14:07:38 -0600

    added guess and if message

--- project/script.js
+++ project/script.js
@@ -11,6 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +20,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
   }
 });
