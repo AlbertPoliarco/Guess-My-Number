6b928b60bd90efa21c33113dd1b16fd81d4b8bb9
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-03-08 14:36:24 -0600

    added color green for correct guess

--- project/script.js
+++ project/script.js
@@ -20,17 +20,33 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    //  When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //  When guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High';
-    score = score - 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+    //  When guess is too high
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low';
-    score = score - 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
   }
 });
